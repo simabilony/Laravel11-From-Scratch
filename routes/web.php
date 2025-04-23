3be5155d30a3dc85a6fce3207771dbbc9243bcc2
Sima Bilony <www.simabilony@gmail.com>
2025-04-23 22:32:40 +0300

    Blade_css_asset

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+Route::view('/', 'home')->name('home');
+Route::view('contact', 'contact')->name('contact');
+Route::view('about', 'about')->name('about');
